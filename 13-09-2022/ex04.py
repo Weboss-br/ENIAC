6423d83a7c70ccedfc8cdc15fe0cf6b1e08338f0
Vander Luis Lauschner <109189009+Vanderlaus@users.noreply.github.com>
2022-09-13 20:34:14 -0300

    ex05
    ex05

---
+++ 13-09-2022/ex04.py
@@ -0,0 +1,24 @@
+#crie uma variável com valor fixo esta variável deve conter um numero que não interfira na decisão de um índice
+#crie uma variável recebendo o conceito de polimorfismo com o sinal de igual.
+
+#crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabeçalho adicione quebra de linha, ao final.
+
+#crie um laço de repetição, este laço deve solicitar ao usuário para digitar um número por 3 vezes..
+#Dentro do laço, crie a capacidade de soma entre esses números digitados, eles devem ser atributos de soma.
+
+#crie uma função de impressão usando máscara de substituição e imprima de forma descritiva a soma desses números.
+
+#crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string utilize como rodapé, definindo o fim do laço de repetição.
+
+#crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string cabeçalho início estrutura de decisão adicione quebra de linha, ao final.
+
+#crie uma estrutura de decisão falando se a soma desses números é maior que 10, menor que 10, igual a dez, diferente de 10.
+
+#crie uma função de impressão usando interpolação e aplique a variável de polimorfismo antes e depois da string utilize como rodapé, definindo o fim da estrutura de decisão.
+
+constante = 8
+
+poli = "="*20
+
+print(f"\n {poli} CABECALHO {poli} \n")
+
--- 13-09-2022/exercicio4.py
+++
@@ -1,5 +0,0 @@
-numero = int(input("Digite um número: "))
-
-resultado = numero % 2
-
-print("{} dividido por 2 = {}".format(numero, resultado))
