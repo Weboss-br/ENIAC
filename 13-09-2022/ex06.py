e6975d87b3c29b50f2e7eaf9d503c83e8f832b4d
Thiago Augusto França <thiagoaugusto.franca@hotmail.com>
2022-09-13 20:25:07 -0300

    Change log
    - ex06 changed value to 1501 to run to 1500 in the for loop after running the code.

--- 13-09-2022/ex06.py
+++ 13-09-2022/ex06.py
@@ -9,8 +9,8 @@
 
 border = "="*5
 print(f"{border} CABEÇALHO {border}\n")
-for i in range(0, 1500, 2):
+for i in range(0, 1501, 2):
     print(i)
 
-print("Parabens !!! Você conseguiu.")
+print("Parabens !!! Você conseguiu.\n")
 print(f"{border} RODAPÉ {border}\n")
