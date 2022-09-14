fe2144628b36190de29439a697cc54b67f2c5db9
JoaoCasali <joaovitorcasali@gmail.com>
2022-09-14 20:23:31 -0300

    Correções exercicios 13/09/22

--- 13-09-2022/ex01.py
+++ 13-09-2022/ex01.py
@@ -1,7 +1,7 @@
 agua = int(input('quantos copos de água você bebeu hoje? '))
 resto = agua % 2
 
-print(f'sobrou {resto}')
+print('sobrou {}'.format(resto))
 
 if resto == 0:
     print('nº par')
--- 13-09-2022/ex02.py
+++ 13-09-2022/ex02.py
@@ -3,6 +3,6 @@ igual = '='*5
 print(f'\n {igual} Que dia é hoje? {igual} \n')
 
 for i in range(0, 10):
-    print('dia dos garotes de programa')
+    print(f'{i}º dia dos garotes de programa')
 
 print(f'\n {igual} Parabéns {igual} \n')
