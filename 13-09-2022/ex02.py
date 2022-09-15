3bbe4fce0610c5c21b9dc621fcfb039475440049
Luiza Bissoni <113144012+LuizaBissoni@users.noreply.github.com>
2022-09-14 21:49:03 -0300

    luiza

--- 13-09-2022/ex02.py
+++ 13-09-2022/ex02.py
@@ -2,7 +2,7 @@ igual = '='*5
 
 print(f'\n {igual} Que dia é hoje? {igual} \n')
 
-for i in range(0, 10):
+for i in range(1, 11):
     print(f'{i}º dia dos garotes de programa')
 
 print(f'\n {igual} Parabéns {igual} \n')
